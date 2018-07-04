a14f78da82554562cd87ea2443a1ead0bf0565fb
maksimn <maksim1989sovunion@yandex.ru>
2018-07-04 18:22:46 +0300

    class SomeHorizontalLayout extends ViewGroup

--- app/src/main/java/ru/yandex/yamblz/viewgroups/SomeHorizontalLayout.java
+++ app/src/main/java/ru/yandex/yamblz/viewgroups/SomeHorizontalLayout.java
@@ -1,4 +1,15 @@
 package ru.yandex.yamblz.viewgroups;
 
-public class SomeHorizontalLayout {
+import android.content.Context;
+import android.view.ViewGroup;
+
+public class SomeHorizontalLayout extends ViewGroup {
+    public SomeHorizontalLayout(Context context) {
+        super(context);
+    }
+
+    @Override
+    public void onLayout(boolean changed, int l, int t, int r, int b) {
+
+    }
 }
