2ec3f125156e1668486f2e1e7bf9f00cce166959
maksimn <maksim1989sovunion@yandex.ru>
2018-07-04 21:16:11 +0300

    SomeHorizontalLayout.LayoutParams base

--- app/src/main/java/ru/yandex/yamblz/viewgroups/SomeHorizontalLayout.java
+++ app/src/main/java/ru/yandex/yamblz/viewgroups/SomeHorizontalLayout.java
@@ -1,6 +1,7 @@
 package ru.yandex.yamblz.viewgroups;
 
 import android.content.Context;
+import android.util.AttributeSet;
 import android.view.ViewGroup;
 
 public class SomeHorizontalLayout extends ViewGroup {
@@ -8,8 +9,30 @@ public class SomeHorizontalLayout extends ViewGroup {
         super(context);
     }
 
+    public SomeHorizontalLayout(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public SomeHorizontalLayout(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
     @Override
     public void onLayout(boolean changed, int l, int t, int r, int b) {
 
+    }
+
+    public static class LayoutParams extends MarginLayoutParams {
+        public LayoutParams(Context c, AttributeSet attrs) {
+            super(c, attrs);
+        }
+
+        public LayoutParams(int width, int height) {
+            super(width, height);
+        }
+
+        public LayoutParams(ViewGroup.LayoutParams source) {
+            super(source);
+        }
     }
 }
