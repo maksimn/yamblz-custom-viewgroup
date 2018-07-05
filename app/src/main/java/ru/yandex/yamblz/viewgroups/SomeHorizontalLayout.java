67031908ac0c04ad3652460f76154fafd554eea2
maksimn <maksim1989sovunion@yandex.ru>
2018-07-05 22:30:52 +0300

    ?

--- app/src/main/java/ru/yandex/yamblz/viewgroups/SomeHorizontalLayout.java
+++ app/src/main/java/ru/yandex/yamblz/viewgroups/SomeHorizontalLayout.java
@@ -1,7 +1,6 @@
 package ru.yandex.yamblz.viewgroups;
 
 import android.content.Context;
-import android.graphics.Rect;
 import android.util.AttributeSet;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,9 +8,6 @@ import android.widget.RemoteViews;
 
 @RemoteViews.RemoteView
 public class SomeHorizontalLayout extends ViewGroup {
-
-    /** These are used for computing child frames based on their gravity. */
-    private final Rect mTmpChildRect = new Rect();
 
     public SomeHorizontalLayout(Context context) {
         super(context);
